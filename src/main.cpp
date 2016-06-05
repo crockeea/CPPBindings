f36d51eaae7502e124d7ab86b218378b8acb44f2
Eric <ecrockett0@gmail.com>
2016-06-05 10:44:16 -0400

    Working example with C++

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "header.h"
-#include "stdio.h"
 #include <iostream>
 
 extern "C" void helloC(hInt_t x, hInt_t q)
@@ -7,6 +6,5 @@ extern "C" void helloC(hInt_t x, hInt_t q)
   Zq::q = q;
   Zq y;
   y = x;
-  printf("Hello from C world! %" PRId64 " = %" PRId64 " mod %" PRId64 "\n", x, y.x, q);
-  //std::cout << "Hello from C world! " << x << " = " << y.x << " mod " << q << "\n";
+  std::cout << "Hello from C world! " << x << " = " << y.x << " mod " << q << "\n";
 }
